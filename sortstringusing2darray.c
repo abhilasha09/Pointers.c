cf06f253e7b850a16295fb0d08a2aa79b9763e5a
abhilasha09 <46891660+abhilasha09@users.noreply.github.com>
2019-06-18 15:06:43 +0530

    Update sortstringusing2darray.c

--- sortstringusing2darray.c
+++ sortstringusing2darray.c
@@ -1,34 +1,37 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-void input(int n,char a[n][100])
+int input(int n,char a[n][100])
 {
-    printf("Enter the strings");
-    for(int i=0;i<n;i++)
-    {
-        scanf("%s",a[i]);
-    }
+	for(int i=0;i<n;i++)
+	{
+		printf("Word %d:\n",i+1);
+		scanf("%s",a[i]);
+	}
 }
-void comp(const void *p1, const void *p2)
+int comp(const void *p,const void *q)
 {
-    return strcmp(* (char * const *) p1, * (char * const *) p2);
-
+                           
+    const char *x = p;
+    const char *y = q;
+    int val = strcmp(x,y);
+    return val;
 }
 void output(int n,char a[n][100])
 {
-    printf("The sorted string is\n");
-    for(int i=0;i<n;i++)
-    {
-        printf("%s\n",a[i]);
-    }
+	printf("The sorted string is\n");
+	for(int i=0;i<n;i++)
+	{
+		printf("%s\n",a[i]);
+	}
 }
 void main()
 {
-    int n;
-    printf("Enter the no.of.strings");
-    scanf("%d",&n);
-    char a[n][100];
-    input(n,a);
-    qsort(a,n,sizeof(char),comp);
-    output(n,a);
+	int n;
+	printf("Enter n");
+	scanf("%d",&n);
+	char a[n][100];
+	input(n,a);
+	qsort(a,n,100,comp);
+	output(n,a);
 }
