020e60896a4f93c7030ac158b5a1546816ddcacd
abhilasha09 <46891660+abhilasha09@users.noreply.github.com>
2019-06-18 16:53:50 +0530

    Update sortstringsusingpointers.c

--- sortstringsusingpointers.c
+++ sortstringsusingpointers.c
@@ -1,39 +1,48 @@
-    #include <stdio.h>
-    #include <string.h>
-    #include<stdlib.h>
-    void main()
-    {
-        int n;
-         printf("Enter the value of n \n");
-        scanf("%d", &n);
-        
-        char *a[n],*temp;
-        int i, j;
- 
-        printf("Enter words");
- 
-        for (i = 0; i < n; i++) 
-        {
-            a[i]=malloc(100);
-            scanf("%s",a[i]);
-        }
- 
-        for (i = 0; i < n - 1 ; i++)
-        {
-            for (j = i + 1; j < n; j++)
-            {
-                if (strcmp(a[i],a[j]) > 0) 
-                {
-                    temp=a[i];
-                    a[i]=a[j];
-                    a[j]= temp;
-                }
-            }
-        }
-        printf("The sorted string is\n");
-        for (i = 0; i < n; i++) 
-        {
-            printf("%s\n",a[i]);
-        }
- 
-    }
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int input(int n,char *a[n])
+{
+	char temp[100];
+	for(int i=0;i<n;i++)
+	{
+		printf("Word %d:\n",i+1);
+		scanf("%s",temp);
+		int l=strlen(temp);
+		printf("%d\n",l);
+		a[i]=malloc(l+1);
+		a[i]=temp;
+		printf("%s\n",a[i]);
+	}
+	printf("Sa,ple\n");
+	for(int i=0;i<n;i++)
+	{
+		printf("%s\n",a[i]);
+	}
+}
+int comp(const void *p,const void *q)
+{
+                           
+    const char *x = p;
+    const char *y = q;
+    int val = strcmp(x,y);
+    return val;
+}
+void output(int n,char *a[n])
+{
+	printf("The sorted string is\n");
+	for(int i=0;i<n;i++)
+	{
+		printf("%s\n",a[i]);
+	}
+}
+void main()
+{
+	int n;
+	printf("Enter n");
+	scanf("%d",&n);
+	char *a[n];
+	input(n,a);
+	qsort(a,n,sizeof(char *),comp);
+	output(n,a);
+}
